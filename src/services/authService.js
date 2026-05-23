0bb49fa2bf643f9091e773e8abf059c3d496a1c5
Shaik Noorjahan sameera <shaiknoorjahansameera@gmail.com>
2026-05-23 17:57:06 +0530

    connect frontend to backend API

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,47 +1,39 @@
+const API = import.meta.env.VITE_API_URL;
+
+// REGISTER USER
 export async function registerUser(userData) {
+  const response = await fetch(`${API}/api/auth/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(userData)
+  });
 
-  const response = await fetch(
-    "http://localhost:5000/api/auth/register",
-    {
-      method: "POST",
-
-      headers: {
-        "Content-Type": "application/json"
-      },
-
-      body: JSON.stringify(userData)
-    }
-  )
-
-  const data = await response.json()
+  const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(data.message)
+    throw new Error(data.message);
   }
 
-  return data
+  return data;
 }
 
+// LOGIN USER
 export async function loginUser(userData) {
+  const response = await fetch(`${API}/api/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(userData)
+  });
 
-  const response = await fetch(
-    "http://localhost:5000/api/auth/login",
-    {
-      method: "POST",
-
-      headers: {
-        "Content-Type": "application/json"
-      },
-
-      body: JSON.stringify(userData)
-    }
-  )
-
-  const data = await response.json()
+  const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(data.message)
+    throw new Error(data.message);
   }
 
-  return data
+  return data;
 }
